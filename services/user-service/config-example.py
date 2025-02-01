5bb940a754cb8131f6ad73989467961d6fdfa020
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-01-31 23:28:19 -0600

    deleted

--- services/user-service/config-example.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SECRET_KEY = "your secret key"
-    SQLALCHEMY_DATABASE_URI = "database uri"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
-
-config = Config()
--- services/user-service/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv()
-    SQLALCHEMY_DATABASE_URI = 
