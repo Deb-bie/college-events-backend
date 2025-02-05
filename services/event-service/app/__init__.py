bc8b001235034f4bfef6c3d649b75b87c7d4a3bc
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-02-04 22:23:41 -0600

    main app

---
+++ services/event-service/app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from .routes import register_routes
+from .utils import db, migrate
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+
+    # Load configuration
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # register blueprints
+    register_routes(app)
+
+    with app.app_context():
+        # create database tables if they don;t exist
+        db.create_all()
+
+    return app
+
